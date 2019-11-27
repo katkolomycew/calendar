a33659977fc3026b143ac5c78d21a37c95aac620
Kat Kolomycew <kat.kolomycew@Nology.local>
2019-11-27 11:54:56 +0000

    summary rendering on page

--- src/App.js
+++ src/App.js
@@ -2,8 +2,8 @@ import React, { Component } from "react";
 import "./App.css";
 
 class App extends Component {
-  constructor(props) {
-    super(props);
+  constructor() {
+    super();
     this.state = {
       items: []
     };
@@ -13,17 +13,31 @@ class App extends Component {
     fetch(
       "https://www.googleapis.com/calendar/v3/calendars/nology.io_5smheaincm2skd1tcmvv7m37d8@group.calendar.google.com/events?key=AIzaSyDZCnzCqZcunGmoYxYOuuCOGrufEIO17ME"
     )
-      .then(calendar => {
-        return calendar.json();
+      .then(results => {
+        return results.json();
       })
       .then(data => {
-        console.log(data);
+        this.setState({ items: data.items });
+        console.log(data.items);
+        console.log(data.items[4].start);
+        // let items = data.items.map(item => {
+        //   let id = new Date(item.start.date);
+        //   let startDate = item.start.date;
+        // });
+        // return (
+        //   <div key={item.id}>
+        //     <p>{item.startDate}</p>
+        //   </div>
+        // );
       });
   }
 
   render() {
     return (
       <div className="container">
+        {this.state.items.map(item => {
+          return <p>{item.summary}</p>;
+        })}
         <div className="body">hgchgfcgh</div>
       </div>
     );
